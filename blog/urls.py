482d7e6ba713f2c31c0b1d591ba5d48151c3cec8
Anniekobia <kobiaannunziata94@gmail.com>
2020-05-08 15:10:40 +0300

    Added template path

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('news', views.news, name='news'),
+    path('blogposts', views.post_list, name='post_list'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'blog/home.html', {})
+
+def about(request):
+    return render(request, 'blog/about.html', {})
+
+def news(request):
+    return render(request, 'blog/news.html', {})
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
